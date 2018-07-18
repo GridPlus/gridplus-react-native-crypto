46e0b99ecd7166112dcf23fc5399d36057b8b0c9
Alex Miller <asmiller1989@gmail.com>
2018-07-18 13:32:59 -0500

    Replaces sjcl with pure javascript crypto functionality

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const sha256 = require('./sha256.js');
+
+class ReactNativeCrypto {
+
+  constructor(e) {
+    // Hash whatever the entropy provided is and use that hash as the entropy for sjcl
+    const shaObj = new sha256('SHA-256', 'TEXT');
+    shaObj.update(e);
+    this.entropy = shaObj.getHash('HEX');
+    this.count = 0;
+  }
+
+  get32RandomBytes() {
+    const shaObj = new sha256('SHA-256', 'TEXT');
+    const r = this.createHash(String(this.count * new Date().getTime()));
+    shaObj.update(`${this.entropy}${r}`);
+    this.count += 1;
+    return shaObj.getHash('HEX');
+  }
+
+  // Return 32 bytes of entropy
+  generateEntropy () {
+    return this.get32RandomBytes();
+  }
+
+  // Return n bytes of entropy
+  randomBytes (n) {
+    const numHashes = Math.ceil(n / 32);
+    let b = '';
+    for (let i = 0; i < numHashes; i++) {
+      b += this.get32RandomBytes();
+    }
+    return b.slice(0, n*2);
+  }
+
+  createHash (x) {
+    const shaObj = new sha256('SHA-256', 'TEXT');
+    shaObj.update(x);
+    return shaObj.getHash('HEX');
+  }
+
+}
+
+export default ReactNativeCrypto;
